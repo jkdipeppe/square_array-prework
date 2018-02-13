b2b8bde723f826bca49686ae4d62b6e17486d89f
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-02-13 04:35:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   array.each do |num|
     array_new = []
-    squared = num.power(num)
+    squared = num ** num
     array_new.push(squared)
   end
 end
